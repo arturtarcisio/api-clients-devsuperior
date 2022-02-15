9cd9699f98ba9bdcfefe7957cb9f40493e3f704c
Artur Tarcisio <arturtarcisio1@gmail.com>
2022-02-14 21:24:09 -0300

    Project created

---
+++ src/test/java/io/github/arturtcs/apiclients/ApiClientsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.arturtcs.apiclients;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiClientsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
